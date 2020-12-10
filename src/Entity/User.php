0ac63d0743ba85493f0f3b0decc2e4f36f6931ed
Marius <58405345+MeriusTz@users.noreply.github.com>
2020-12-10 16:28:11 +0100

    Inscription Fonctionnel/Connexion à Terminer

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,14 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"NameUser"}, message="There is already an account with this NameUser")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -20,19 +21,25 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=22)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $NameUser;
 
     /**
-     * @ORM\Column(type="string", length=32)
+     * @ORM\Column(type="json")
      */
-    private $Email;
+    private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string The hashed password
+     * @ORM\Column(type="string")
      */
-    private $Password;
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Email;
 
     /**
      * @ORM\Column(type="integer")
@@ -40,15 +47,12 @@ class User
     private $Score;
 
     /**
-     * @ORM\OneToMany(targetEntity=Character::class, mappedBy="IdUser")
+     * @param mixed $id
      */
-    private $idUser;
-
     public function __construct()
     {
-        $this->idUser = new ArrayCollection();
+        $this->Score = 0;
     }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -66,6 +70,67 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->NameUser;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
     public function getEmail(): ?string
     {
         return $this->Email;
@@ -78,18 +143,6 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
-    {
-        return $this->Password;
-    }
-
-    public function setPassword(string $Password): self
-    {
-        $this->Password = $Password;
-
-        return $this;
-    }
-
     public function getScore(): ?int
     {
         return $this->Score;
@@ -98,36 +151,6 @@ class User
     public function setScore(int $Score): self
     {
         $this->Score = $Score;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Character[]
-     */
-    public function getIdUser(): Collection
-    {
-        return $this->idUser;
-    }
-
-    public function addIdUser(Character $idUser): self
-    {
-        if (!$this->idUser->contains($idUser)) {
-            $this->idUser[] = $idUser;
-            $idUser->setIdUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeIdUser(Character $idUser): self
-    {
-        if ($this->idUser->removeElement($idUser)) {
-            // set the owning side to null (unless already changed)
-            if ($idUser->getIdUser() === $this) {
-                $idUser->setIdUser(null);
-            }
-        }
 
         return $this;
     }
